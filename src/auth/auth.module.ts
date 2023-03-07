9a5786bc64a75a9e6a202b4d5f3f5423248a8466
seuungkei <seuungkei@gmail.com>
2023-03-07 17:03:13 +0900

    jwt를 이용한 로그인 기능 구현

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { CatsModule } from 'src/cats/cats.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1y' },
+    }),
+    forwardRef(() => CatsModule),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
